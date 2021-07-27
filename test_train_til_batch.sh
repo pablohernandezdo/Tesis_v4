ec686da961b9a10c5e0f13472e6cbe127becd79b
Pablo Hernandez <pablo.hernandezdo.13@sansano.usm.cl>
2021-07-27 05:17:08 -0400

    Train on device 3

--- test_train_til_batch.sh
+++ test_train_til_batch.sh
@@ -4,7 +4,7 @@
 echo "Training model Ann_4k_2k, lr = 1e-4, epochs = 30, batch_size = 256"
 python train_til_batch.py \
         --lr 1e-4 \
-        --device 2 \
+        --device 3 \
         --epochs 30 \
         --batch_size 256 \
         --final_batch 100\

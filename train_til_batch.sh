4a4c05046382ce6f07fb407cfd9fc7b58a92e3f6
Pablo Hernandez <pablo.hernandezdo.13@sansano.usm.cl>
2021-08-01 22:58:56 -0400

    Train enough epochs

--- train_til_batch.sh
+++ train_til_batch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # ANN_6K_3K
-echo "Training model ANN_6k_6k, lr = 1e-6, epochs = 30, batch_size = 256"
+echo "Training model ANN_6k_6k, lr = 1e-6, epochs = 100, batch_size = 256"
 python train_til_batch.py \
         --lr 1e-6 \
         --device 3 \
-        --epochs 30 \
+        --epochs 100 \
         --batch_size 256 \
         --final_batch 22000 \
         --earlystop 0 \
@@ -18,11 +18,11 @@ python train_til_batch.py \
         --val_path "Data/TrainReady/val_zeros.npy"
 
 # CNN
-echo "Training model CNN, lr = 1e-3, epochs = 30, batch_size = 256"
+echo "Training model CNN, lr = 1e-3, epochs = 100, batch_size = 256"
 python train_til_batch.py \
         --lr 1e-3 \
         --device 3 \
-        --epochs 30 \
+        --epochs 100 \
         --batch_size 256 \
         --final_batch 4472 \
         --earlystop 0 \
@@ -35,11 +35,11 @@ python train_til_batch.py \
         --val_path "Data/TrainReady/val_zeros.npy" 
 
 # CRED
-echo "Training model CRED, lr = 1e-5, epochs = 30, batch_size = 256"
+echo "Training model CRED, lr = 1e-5, epochs = 100, batch_size = 256"
 python train_til_batch.py \
         --lr 1e-5 \
         --device 3 \
-        --epochs 30 \
+        --epochs 100 \
         --batch_size 256 \
         --final_batch 30000 \
         --earlystop 0 \

84f7806fc0b94ea2b72e84413e9eb0f4690cd043
Pablo Hernandez <pablo.hernandezdo.13@sansano.usm.cl>
2021-11-22 20:14:00 -0300

    Update gitignore, add tiempos paper

---
+++ TIMEPOSPAPER.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# ANN_6K_3K
+echo "Training model ANN_6k_6k, lr = 1e-6, epochs = 100, batch_size = 256"
+python train_til_batch.py \
+       --lr 1e-6 \
+       --device 3 \
+       --epochs 100 \
+       --batch_size 256 \
+       --final_batch 22000 \
+       --earlystop 0 \
+       --eval_iter 30 \
+       --model_folder 'modelos_batches'  \
+       --classifier ANN \
+       --model_name ANN_TIEMPO_PAPER \
+       --dataset_name "STEAD-ZEROS" \
+       --train_path "Data/TrainReady/train_zeros.npy" \
+       --val_path "Data/TrainReady/val_zeros.npy"
+
+# CNN
+echo "Training model CNN, lr = 1e-3, epochs = 100, batch_size = 256"
+python train_til_batch.py \
+        --lr 1e-3 \
+        --device 3 \
+        --epochs 100 \
+        --batch_size 256 \
+        --final_batch 4472 \
+        --earlystop 0 \
+        --eval_iter 30 \
+        --model_folder 'modelos_batches'  \
+        --classifier CNN \
+        --model_name CNN_TIEMPO_PAPER \
+        --dataset_name "STEAD-ZEROS" \
+        --train_path "Data/TrainReady/train_zeros.npy" \
+        --val_path "Data/TrainReady/val_zeros.npy" 
+
+# CRED
+echo "Training model CRED, lr = 1e-5, epochs = 100, batch_size = 256"
+python train_til_batch.py \
+        --lr 1e-5 \
+        --device 3 \
+        --epochs 100 \
+        --batch_size 256 \
+        --final_batch 30000 \
+        --earlystop 0 \
+        --eval_iter 30 \
+        --model_folder 'modelos_batches'  \
+        --classifier CRED \
+        --model_name CRED_TIEMPO_PAPER \
+        --dataset_name "STEAD-ZEROS" \
+        --train_path "Data/TrainReady/train_zeros.npy" \
+        --val_path "Data/TrainReady/val_zeros.npy" 
